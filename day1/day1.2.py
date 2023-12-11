306217614f1e00c9eaa50302f48360d2cd997fda
Arthur Reis Bello <arthurrrbello@hotmail.com>
2023-12-11 18:47:14 -0300

    fixed day1 part two

--- day1/day1.2.py
+++ day1/day1.2.py
@@ -3,9 +3,13 @@ count = 0
 numbers = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 while 1:
     try:
+        # reading val
         count+=1
         val = input()
+        valBackwards = val[::-1]
         print(str(count).rjust(4)+ ' - '+val.ljust(60) + ' ', end='')
+
+        # forward calculation
         for i in range(len(val)):
             for number in numbers:
                 if val[i:i+len(number)] == number:
@@ -18,13 +22,19 @@ while 1:
                 calibration = val[i]
                 break
         
-        val = val[::-1]
-        for i in range(len(val)):
-            if val[i].isnumeric():
-                calibration += val[i]
+
+        # backward calculation
+        for i in range(len(valBackwards)):
+            for number in numbers:
+                if valBackwards[i:i+len(number)] == number[::-1]:
+                    valBackwards = valBackwards[0:i] + str(numbers[number]) + valBackwards[i+len(number):]
+        
+
+        for i in range(len(valBackwards)):
+            if valBackwards[i].isnumeric():
+                calibration += valBackwards[i]
                 break
         sum+=int(calibration)
-        val = val[::-1]
         print(''.join(val).ljust(60) + ': ' + str(calibration) +' '+str(sum))
     except EOFError:
         break
